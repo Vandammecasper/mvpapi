ceb2689b7db47b0c0fa66649d84b66db1d9dd353
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-21 21:44:16 +0200

    added specific origins

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: ['*'],
+  origin: ['https://mvpvote.vercel.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
