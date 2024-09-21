6b4bd8fe22cff0cc042055717d680269bedb224d
Casper Van Damme <casper.van.damme@student.howest.be>
2024-09-21 20:32:12 +0200

    added allowheaders

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: ['*'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  credentials: false,
+  origin: ['https://mvpvote.vercel.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
 });
   await app.listen(3000);
   console.info(`Server is running on: ${await app.getUrl()}`)
